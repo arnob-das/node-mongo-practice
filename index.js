7704a8bcadd86da01028016098b3180992b8aec2
arnob-das <contact.arnobdas@gmail.com>
2021-06-21 13:49:05 +0600

    another api opened and exploring different types of response sending process

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ import express from 'express'
 const app = express()
 
 // HTTP Request (path, callback function)
+
 app.get('/', (req, res) => {
-
     // res.send("I Can Run Node Server. Yay !!!!")
-
     const fruit = {
         product: 'Ginger',
         price: 220

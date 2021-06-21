528478b0f88c739db3603e3888d6ede52b12fdc1
arnob-das <contact.arnobdas@gmail.com>
2021-06-21 13:21:04 +0600

    Node - Exoress Application Started

---
+++ README.md
@@ -0,0 +1,2 @@
+# node-mongo-practice
+# node-mongo-practice
---
+++ index.js
@@ -0,0 +1,14 @@
+// Creates an Express application. The express() function is a top-level function exported by the express module.
+
+const express = require('express')
+const app = express()
+
+// HTTP Request (path, callback function)
+
+app.get('/', (req, res) => {
+    res.send("Thank You For Calling Me !!!")
+})
+
+// port (port number, callback function)
+
+app.listen(3000, () => console.log("Listening to port 3000"))
